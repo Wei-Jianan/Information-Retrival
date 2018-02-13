9710edbc346f50dad02a730ad6953412c089f790
Jianan Wei <weij@TCD.ie>
2018-02-13 14:02:09 +0000

    Class Utill are done, which is responsible to build the directory to contain the raw doc to be downloaded and conpressed.

--- cran-sample/src/main/java/com/sample/Utils.java
+++ cran-sample/src/main/java/com/sample/Utils.java
@@ -1,36 +1,124 @@
 package com.sample;
 
+import org.apache.commons.io.FileUtils;
+import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
+import org.codehaus.plexus.logging.console.ConsoleLoggerManager;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.Arrays;
+import java.util.Comparator;
+//import org.apache.commons.io.FileUtils;
 
 
 public class Utils {
-    private final static Path TEMP_DIR = Paths.get(new File("src/temp").getAbsolutePath());
-    private final static Path INDEX_DIR = Paths.get(TEMP_DIR.toString(), "index");
-    private final static Path RAW_DOC_DIR = Paths.get(TEMP_DIR.toString(), "raw_doc");
+    private final static File TEMP_DIR = new File("src/temp");
+    private final static File INDEX_DIR = TEMP_DIR.toPath().resolve("index").toFile();
+    private final static File RAW_DOC_DIR = TEMP_DIR.toPath().resolve("raw").toFile();
+    private final static URL url;
 
-    public static void initialize(){
-//        System.out.println( RAW_DOC_DIR);
-////        System.out.println( DOCS_DIR);
-//        ClassLoader classLoader = new Object.getClass().getClassLoader();
-//        File file = new File(classLoader.getResource("somefile").getFile());
-        System.out.println(resourcesDirectory.getAbsolutePath());
-    }
-    public static void clear(Path toBeDeleted) {
+    static {
         try {
-
+            url = new URL("http://ir.dcs.gla.ac.uk/resources/test_collections/cran/cran.tar.gz");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new Error(e);
         }
+
     }
 
-    boolean deleteDirectory(File directoryToBeDeleted) {
-        File[] allContents = directoryToBeDeleted.listFiles();
-        if (allContents != null) {
-            for (File file : allContents) {
-                deleteDirectory(file);
+    //    private final static Path RAW_DOC_DIR = TEMP_DIR.resolve("raw_doc");
+    public static void initialize() throws IOException {
+//        System.out.println( RAW_DOC_DIR);
+//        System.out.println( DOCS_DIR);
+//        File file = new File(classLoader.getResource("somefile").getFile());
+        if (TEMP_DIR.isDirectory()) {
+            clear(TEMP_DIR);
+        }
+        TEMP_DIR.mkdir();
+        INDEX_DIR.mkdirs();
+        RAW_DOC_DIR.mkdirs();
+
+        File rawDocFile = downloadFromUrltoDir(url, RAW_DOC_DIR);
+        decompress(rawDocFile);
+        delate(rawDocFile);
+
+        System.out.println("\n \nthe director " +RAW_DOC_DIR.toString() + "contains:\n" );
+        Arrays.stream(RAW_DOC_DIR.list())
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    public static void delate(File toDeleteFile) {
+        toDeleteFile.delete();
+        System.out.println(toDeleteFile.toString() + " are deleted.");
+    }
+
+    public static void decompress(File toDecompressFile) {
+        TarGZipUnArchiver ua = new TarGZipUnArchiver();
+        ConsoleLoggerManager manager = new ConsoleLoggerManager();
+        manager.initialize();
+        ua.enableLogging(manager.getLoggerForComponent("bla"));
+        ua.setSourceFile(toDecompressFile);
+        ua.setDestDirectory(toDecompressFile.getParentFile());
+        ua.extract();
+        System.out.println(toDecompressFile.toString() + " are decompressed.");
+    }
+
+
+    public static boolean clear(File toBeDeleted) {
+        File[] contentsInDir = toBeDeleted.listFiles();
+        if (contentsInDir != null) {
+            for (File item : contentsInDir) {
+                clear(item);
             }
         }
-        return directoryToBeDeleted.delete();
+        System.out.println(toBeDeleted.toString() + " are cleared.");
+        return toBeDeleted.delete();
+    }
+
+    public static boolean clear(Path toBeDeleted) {
+        File f = new File(toBeDeleted.toString());
+        return clear(f);
+    }
+
+    public static void clearFP(File toBeDeleted) throws IOException {
+//        Path pathToBeDEleted = Paths.get(toBeDeleted.getAbsolutePath());
+//        Files
+//                .walk(pathToBeDEleted)
+//                .sorted(Comparator.reverseOrder())
+//                .map(Path::toFile)
+//                .forEach(File::delete);
+//        FileUtils.forceDelete(new File(destination))
+    }
+
+    public static File downloadFromUrltoDir(URL fromUrl, File toDir) {
+        String rawDocName = fromUrl.toString().substring(fromUrl.toString().lastIndexOf("/") + 1);
+        File toFile = new File(toDir, rawDocName);
+        try {
+
+            FileUtils.copyURLToFile(fromUrl, toFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        //another way to download
+        System.out.println(toFile.getAbsoluteFile().toString() + " are downloaded to " + toDir.toString());
+        return toFile;
+    }
+
+    public static void main(String[] args) {
+        try {
+            initialize();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
