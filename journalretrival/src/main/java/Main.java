876eff86a7dc41d9aaaadd88507f65630d9f6708
Wei-Jianan <weij@tcd.ie>
2018-03-19 11:55:41 +0000

    method initialize method done

--- journalretrival/src/main/java/Main.java
+++ journalretrival/src/main/java/Main.java
@@ -1,6 +1,33 @@
+import org.apache.commons.cli.*;
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println("hello world!");
+        Options options = new Options();
+
+        Option jsons = new Option("j", "jsons", true, "json files directory containing all raw document");
+        jsons.setRequired(true);
+        options.addOption(jsons);
+
+        Option queries = new Option("q", "queries", true, "queries directory containing 40 topics");
+        queries.setRequired(true);
+        options.addOption(queries);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("Indexing and searching given 4 journals", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String jsonsDirPath = cmd.getOptionValue("jsons");
+        String queriesDirPath = cmd.getOptionValue("queries");
+        Utils.initialize(true, jsonsDirPath, queriesDirPath);
     }
 }
--- journalretrival/src/main/java/Utils.java
+++ journalretrival/src/main/java/Utils.java
@@ -1,9 +1,50 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.temporal.Temporal;
+import java.util.Comparator;
 
 public class Utils {
     private final static File TEMP_DIR = new File("temp");
     public final static File INDEX_DIR = new File(TEMP_DIR, "index");
-    public final static File RAW_DOC_DIR = new File(TEMP_DIR, "raw");
+    public static File JSONS_DIR = null;
+    public static File QUERIES_FIR = null;
 
+    private static void clearTempDir(boolean clearIndex) {
+        if (clearIndex) {
+            try {
+                clear(TEMP_DIR);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            TEMP_DIR.mkdir();
+            INDEX_DIR.mkdirs();
+        }
+        System.out.println("the director " + INDEX_DIR.toString() + " have been made.");
+    }
+    public static void initialize(boolean clearIndex, String jsonsDirPath, String queriesDirPath) {
+        clearTempDir(clearIndex);
+        JSONS_DIR = new File(jsonsDirPath);
+        QUERIES_FIR = new File(queriesDirPath);
+        if (JSONS_DIR.isDirectory()  && QUERIES_FIR.isFile()) {
+            System.out.println("jsons directory is :" + JSONS_DIR.getAbsolutePath());
+            System.out.println("queries directory is:" + QUERIES_FIR.getAbsolutePath());
+        } else {
+            System.out.println("-j should follow a path to directory\n -j should follow a path to a file");
+        }
+    }
+    private static void clear(File toBeDeleted) throws IOException {
+        Path pathToBeDEleted = Paths.get(toBeDeleted.getAbsolutePath());
+
+        Files.walk(pathToBeDEleted)
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+        System.out.println("the director " + TEMP_DIR.toString() + "have been deleted.");
+        // common-io to delete a directory
+//        FileUtils.forceDelete(new File(destination));
+    }
 
 }
